4cbf86f3b4239bb5f659339c18ca5b2177f7e93e
mbayi-ios <ambayi@lunchbox.io>
2022-04-01 23:25:07 +0300

    Initial Commit

---
+++ Messager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messager
+//
+//  Created by Amby on 01/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
